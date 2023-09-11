bde4a4ea9e8dedbf19dd1b2a9e1fec9af0d46fff
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-09-11 02:07:38 -0400

    update map in place details

--- lib/screens/place_details.dart
+++ lib/screens/place_details.dart
@@ -5,6 +5,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 class PlaceDetailsScreen extends ConsumerWidget {
   const PlaceDetailsScreen({super.key, required this.place});
   final Place place;
+  String get _staticMapImage {
+    final lat = place.location.latitude;
+    final long = place.location.longtitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long&zoom=16&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$long&key=AIzaSyA3k5JPBdg6gPEOEuhM2uSrnnC_pjZtE2w';
+  }
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
@@ -17,7 +23,33 @@ class PlaceDetailsScreen extends ConsumerWidget {
           fit: BoxFit.cover,
           width: double.infinity,
           height: double.infinity,
-        )
+        ),
+        Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 70,
+                  backgroundImage: NetworkImage(_staticMapImage),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.transparent, Colors.black54],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter)),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  alignment: Alignment.center,
+                  child: Text(place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground)),
+                )
+              ],
+            ))
       ]),
     );
   }
